cea1505209e4477af6f6ff6bc8406dd7229d4ca6
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 03:26:07 -0600

    Deleted unneccesary files

--- target/classes/rorgmod/strings/eng/e.py
+++
@@ -1,9 +0,0 @@
-print("RELIC JSON MAKER")
-f = open("newText.txt", "w")
-while 1:
-    print("\nENTER :")
-    x = input()
-    if x == "q":
-        break
-    f.write("  \"rorgmod:"+x+"\": {\n    \"NAME\": \""+x+"\",\n    \"FLAVOR\": \"\",\n    \"DESCRIPTIONS\": [\n      \"\"\n ]\n  },")
-f.close()
